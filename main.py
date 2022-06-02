80e01b2d3e079b7f5bc13ee39e103552683eda8f
Brian Horner <bhorner@suffolk.edu>
2022-06-01 22:42:29 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+Space Invaders Clone
+Author: Brian Horner
+_____________________________
+Tutorial from: TechWithTime
+Link: https://www.youtube.com/watch?v=Q-__8Xw9KTM
+_____________________________
+Edit History:
+6/1/2022 - Initial Version
+"""
+
+# imports
+import pygame
+import os
+import time
+import random
+
+
